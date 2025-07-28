a6f539f827a963a6f67196c2faeeed3130157a12
Louis Aurea <louis@nextstation.cl>
2025-07-28 10:53:32 -0400

    aadding starting

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello World :)")
+from fastapi import FastAPI, Request
+import logging
+
+# Configura los logs
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+@app.post("/webhook")
+async def receive_post(request: Request):
+    body = await request.json()
+    logger.info(f"📩 POST recibido: {body}")
+    return {"message": "Datos recibidos correctamente"}
---
+++ startup.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=/app
+
+
+echo " Iniciando FastAPI..."
+uvicorn app.main:app --host 0.0.0.0 --port 8000
