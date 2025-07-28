4255118cec2473e2bbed14ec6915ad4ea9a21f2e
Louis Aurea <louis@nextstation.cl>
2025-07-28 11:36:38 -0400

    wasyprint log

--- startup.sh
+++ startup.sh
@@ -6,9 +6,21 @@ apt-get update && apt-get install -y \
     libgdk-pixbuf2.0-0 \
     libffi-dev \
     libpq-dev \
+    build-essential \
     python3-dev \
-    build-essential
+    libxml2 \
+    libxslt1.1 \
+    libjpeg-dev \
+    libpng-dev \
+    libpangocairo-1.0-0 \
+    fonts-liberation \
+    fonts-dejavu \
+    fonts-freefont-ttf
+
 
 
 echo " Iniciando FastAPI..."
 uvicorn main:app --host 0.0.0.0 --port 8000
+
+
+python3 -m weasyprint.tool --info
