e1d07488a5ac0be1e3220453096016c0c9b0cb44
Luis <luis1233210e@gmail.com>
2025-08-22 11:35:29 -0400

    adding push

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+from app.api.endpoints.calls import router as router_calls
 from app.api.endpoints.health_check import router as router_check    
 
 api_router = APIRouter()
@@ -8,5 +8,7 @@ api_router = APIRouter()
 api_router.include_router(router_check, tags=["HealthCheck"],
     responses={404: {"description": "Not found"}})
 
+api_router.include_router(router_calls, prefix="/calls", tags=["Calls"],
+    responses={404: {"description": "Not found"}})      
 
 
